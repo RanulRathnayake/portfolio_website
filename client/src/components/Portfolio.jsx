17c39cefc1e65a1e12d1c6eb50ba0488fad87251
RanulRathnayake <109923517+RanulRathnayake@users.noreply.github.com>
2023-06-24 04:32:30 +0530

    redirect project's github links

--- client/src/components/Portfolio.jsx
+++ client/src/components/Portfolio.jsx
@@ -19,7 +19,7 @@ const Portfolio = () => {
           </div>
           <h3>Project Crawlee</h3>
           <p>Under the knowledge of our industry partner "creative hub global" we created Learning Management System for 2nd year software group project in my university</p>
-          <a href="https://github.com/" className="btn">View Project</a>
+          <a href="https://github.com/RushenSamodya/PrimeX_Crawlee_FrontEnd_1.0" className="btn" target='_blanck'>View Project</a>
         </article>
         <article className="portfolio__item">
           <div className="portfolio__item-image">
@@ -28,7 +28,7 @@ const Portfolio = () => {
           <h3>Instagram Clone - MERN Stack Project</h3>
           <p>This clone has all the functionalities such as create profile, create post, 
 user login/logout, follow other users and like and comment to posts.React, NodeJS, ExpressJS and MongoDB are the rechnologiea used</p>
-          <a href="https://github.com/" className="btn">View Project</a>
+          <a href="https://github.com/RanulRathnayake/InstergramClone" className="btn" target='_blanck'>View Project</a>
         </article>
         <article className="portfolio__item">
           <div className="portfolio__item-image">
@@ -36,7 +36,7 @@ user login/logout, follow other users and like and comment to posts.React, NodeJ
           </div>
           <h3>Automated Pill Dispenser</h3>
           <p>That dispenser can provide the relevant oils, medicine pills, and liquids that are mixed with different medicine powders to the patients. We designed this for 1st year hardware group project in my university</p>
-          <a href="https://github.com/" className="btn">View Project</a>
+          <a href="https://github.com/" className="btn" target='_blanck'>View Project</a>
         </article>
         <article className="portfolio__item">
           <div className="portfolio__item-image">
@@ -44,7 +44,7 @@ user login/logout, follow other users and like and comment to posts.React, NodeJ
           </div>
           <h3>Portfolio Website</h3>
           <p>Through this portfolio website, visitors can download my CV to learn more about my qualifications. They can also view my showcased projects and easily contact me via email, Messenger, or WhatsApp.</p>
-          <a href="https://github.com/" className="btn">View Project</a>
+          <a href="https://github.com/RanulRathnayake/portfolio_website" className="btn" target='_blanck'>View Project</a>
         </article>
         <article className="portfolio__item">
           <div className="portfolio__item-image">
@@ -52,7 +52,7 @@ user login/logout, follow other users and like and comment to posts.React, NodeJ
           </div>
           <h3>Chat Application - MERN Stack Project</h3>
           <p>The chat application allows users to send and receive messages in real time, create chat groups or private conversations. It was developed using ReactJS, Node.js and MongoDB</p>
-          <a href="https://github.com/" className="btn">View Project</a>
+          <a href="https://github.com/RanulRathnayake/chatApplication" className="btn" target='_blanck'>View Project</a>
         </article>
         <article className="portfolio__item">
           <div className="portfolio__item-image">
@@ -60,7 +60,7 @@ user login/logout, follow other users and like and comment to posts.React, NodeJ
           </div>
           <h3>PvP Fighting Game</h3>
           <p>The PvP fighting game, developed using JavaScript and HTML, allows two users to engage in a dynamic virtual combat. Both players can moveand attacks to the other player.</p>
-          <a href="https://github.com/" className="btn">View Project</a>
+          <a href="https://github.com/RanulRathnayake/PvP_Fighting_Game" className="btn" target='_blanck'>View Project</a>
         </article>
       </div>
     </section>
